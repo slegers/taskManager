ac1609e6bc156fa3e860a8c73fa9d11b1265cc41
Yanice Slegers <yanice@dumbledore.local>
2017-09-05 22:55:02 +0200

    controller for task and model impl

---
+++ database/migrations/2017_09_05_201752_create_task_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('deadline');
+            $table->string('title',50);
+            $table->string('omschrijving',500);
+            $table->boolean('notificate');
+            $table->boolean('closed');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+}
