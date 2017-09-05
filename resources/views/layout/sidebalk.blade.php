d57e02266bf27694dd20140daa52bb08083c9928
Yanice Slegers <yanice@dumbledore.local>
2017-09-05 23:23:43 +0200

    deel van de layouts gecreeërd

---
+++ resources/views/layout/sidebalk.blade.php
@@ -0,0 +1,37 @@
+<div class="col-sm-3">
+                  <div class="taakBalk"> 
+                  <h3>Taakbalk</h3>
+                  <ul class="tasks">
+                    <li class="task">
+                      <div class="taskHeader">
+                        <form><input type="checkbox"> </form>
+                        <h4 class="taskTitle">Task manager afmaken en dan nog wat tekst toevoegen<br>
+                          <small>Donderdag 15 sept.</small>
+                        </h4>
+                      </div>
+                        <br>
+                        <div class="taskButtons">
+                        <input type="button" value="edit">
+                        <input type="button" value="delete">
+                        </div>
+                      </li>
+                      <li class="task">
+                        <div class="taskHeader">
+                          <form><input type="checkbox"> </form>
+                          <h4 class="taskTitle">Pleinraadweekend voorbereieden <br>
+                            <small>Donderdag 15 sept.</small>
+                         </h4>
+                        </div>
+                          <br>
+                          <div class="taskButtons">
+                          <input type="button" value="edit">
+                          <input type="button" value="delete">      
+                          </div>               
+                          <div class="tags">
+                              Speelplein, werk
+                              </div>
+                        </li>
+                  </ul>
+                  </div>
+                </div>
+            </div>
